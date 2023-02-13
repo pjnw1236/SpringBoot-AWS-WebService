deaaf2e6f59238cbc2e7ec6ce788666442c23d18
pjnw1236 <pjnw1236@gmail.com>
2023-02-13 16:05:46 +0900

    feat: PostsApiController 및 PostsService 생성

---
+++ src/main/java/com/example/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import com.example.domain.posts.PostsRepository;
+import com.example.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
