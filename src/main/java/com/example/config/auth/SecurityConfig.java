667fe4b60833463d1576749fea3483336cc8a008
pjnw1236 <pjnw1236@gmail.com>
2023-02-16 16:33:08 +0900

    feat: 구글 로그인 구현

---
+++ src/main/java/com/example/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.config.auth;
+
+import com.example.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOauth2UserService;
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOauth2UserService);
+
+        return http.build();
+    }
+}
--- src/main/java/com/example/web/IndexController.java
+++ src/main/java/com/example/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.web;
 
+import com.example.config.auth.dto.SessionUser;
 import com.example.service.PostsService;
 import com.example.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/posts/save")
     public String postsSave() {
@@ -21,6 +25,12 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
